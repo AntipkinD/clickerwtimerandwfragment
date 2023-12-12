3263adafbe2ce1781ce20d941861c98564cc311a
mushroomvoodoo <normeeboy@gmail.com>
2023-12-12 09:33:06 +0300

    Добавлены фрагменты

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.clickerwtimerandwfragment"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.clickerwtimerandwfragment"
@@ -33,6 +33,7 @@ android {
 
 dependencies {
 
+    implementation ("androidx.fragment:fragment:1.3.6")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
